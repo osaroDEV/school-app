11a40d8c3fb19de336ddf5ce3793449f1daac778
osarodev <osernon@gmail.com>
2023-06-06 19:01:52 +0100

    add Header menu

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -13,9 +13,25 @@ const Nav = () => {
   return (
     <div
       onClick={handleClick}
-      className='text-5xl bg-[#103356] text-white w-[20%] md:w-[50%] h-full flex justify-center items-center'
+      className='text-5xl bg-[#103356] text-white w-[20%] md:w-[50%] h-full flex justify-center items-center relative'
     >
       {isOpen ? <FiX /> : <FiMenu />}
+      {isOpen && (
+        <ul className='flex flex-col justify-around items-center absolute w-[100vw] h-auto bg-[#103356] bottom-[-210px] left-[-400%]'>
+          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            About
+          </li>
+          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            Academics
+          </li>
+          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            Admissions
+          </li>
+          <li className='indent-[.5rem] border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            Campus Life
+          </li>
+        </ul>
+      )}
     </div>
   );
 };
