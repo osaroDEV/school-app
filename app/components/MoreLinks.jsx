c8a4240a007788022ff0f0dec9808392d392de10
osarodev <osernon@gmail.com>
2023-06-07 09:56:59 +0100

    update MoreLinks

--- app/components/MoreLinks.jsx
+++ app/components/MoreLinks.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState } from 'react';
+import Link from 'next/link';
 import { MdOutlineNavigateNext } from 'react-icons/md';
 
 const MoreLinks = () => {
@@ -17,19 +18,32 @@ const MoreLinks = () => {
       <p>{isMore ? 'Less links' : 'More links'}</p>
       <MdOutlineNavigateNext className='text-[#f1c40f] text-2xl' />
       {isMore && (
-        <ul className='flex flex-col justify-around items-center absolute w-full h-auto bg-[#103356] top-[-220px] left-0'>
-          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            About
-          </li>
-          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            Academics
-          </li>
-          <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            Admissions
-          </li>
-          <li className='indent-[.5rem] border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            Campus Life
-          </li>
+        <ul className='flex flex-col justify-around items-center absolute w-full h-auto bg-[#103356] top-[-265px] left-0'>
+          <Link href='/' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
+              Home
+            </li>
+          </Link>
+          <Link href='/about' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
+              About
+            </li>
+          </Link>
+          <Link href='/academics' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem] '>
+              Academics
+            </li>
+          </Link>
+          <Link href='/admissions' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
+              Admissions
+            </li>
+          </Link>
+          <Link href='/campuslife' className='border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
+              Campus Life
+            </li>
+          </Link>
         </ul>
       )}
     </div>
