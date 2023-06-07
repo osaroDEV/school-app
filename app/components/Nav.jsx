cc2008793c802dcabbb4ff192725ead0cf221ed8
osarodev <osernon@gmail.com>
2023-06-07 09:39:36 +0100

    style nav Links

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -19,28 +19,28 @@ const Nav = () => {
       {isOpen ? <FiX /> : <FiMenu />}
       {isOpen && (
         <ul className='flex flex-col justify-around items-center absolute w-[100vw] h-auto bg-[#103356] bottom-[-260px] left-[-400%]'>
-          <Link href='/'>
-            <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+          <Link href='/' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
               Home
             </li>
           </Link>
-          <Link href='/about'>
-            <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+          <Link href='/about' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
               About
             </li>
           </Link>
-          <Link href='/academics'>
-            <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+          <Link href='/academics' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem] '>
               Academics
             </li>
           </Link>
-          <Link href='/admissions'>
-            <li className='indent-[.5rem] border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+          <Link href='/admissions' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
               Admissions
             </li>
           </Link>
-          <Link href='/campuslife'>
-            <li className='indent-[.5rem] border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+          <Link href='/campuslife' className='border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
+            <li className='indent-[.5rem]'>
               Campus Life
             </li>
           </Link>
