785fb3544e35d3f1a84f11493b84935e1b0b2daa
osarodev <osernon@gmail.com>
2023-06-09 21:49:35 +0100

    make Header fully responsive

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -12,41 +12,51 @@ const Nav = () => {
   };
 
   return (
-    <div
-      onClick={handleClick}
-      className='text-5xl bg-[#103356] text-white w-[20%] md:w-[50%] h-full flex justify-center items-center relative'
-    >
-      {isOpen ? <FiX className='cursor-pointer' /> : <FiMenu className='cursor-pointer' />}
-      {isOpen && (
-        <ul className='flex flex-col justify-around items-center absolute w-[100vw] h-auto bg-[#103356] bottom-[-263px] left-[-400%] md:left-[-620px] md:w-[697px] lg:w-[512px] lg:left-[-410px]'>
-          <Link href='/' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            <li className='indent-[.5rem]'>
-              Home
-            </li>
-          </Link>
-          <Link href='/about' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            <li className='indent-[.5rem]'>
-              About
-            </li>
-          </Link>
-          <Link href='/academics' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            <li className='indent-[.5rem] '>
-              Academics
-            </li>
-          </Link>
-          <Link href='/admissions' className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            <li className='indent-[.5rem]'>
-              Admissions
-            </li>
-          </Link>
-          <Link href='/campuslife' className='border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'>
-            <li className='indent-[.5rem]'>
-              Campus Life
-            </li>
-          </Link>
-        </ul>
-      )}
-    </div>
+    <>
+      <div className='text-5xl bg-[#103356] text-white w-[20%] md:min-w-[84px] xl:w-0 h-full xl:h-0 flex justify-center items-center relative xl:hidden'>
+        <div onClick={handleClick}>
+          {isOpen ? (
+            <FiX className='cursor-pointer' />
+          ) : (
+            <FiMenu className='cursor-pointer' />
+          )}
+        </div>
+        {isOpen && (
+          <ul className='flex flex-col justify-around items-center absolute w-[100vw] h-auto bg-[#103356] bottom-[-263px] left-[-400%] md:left-[-620px] md:w-[697px] lg:w-[512px] lg:left-[-410px]'>
+            <Link
+              href='/'
+              className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'
+            >
+              <li className='indent-[.5rem]'>Home</li>
+            </Link>
+            <Link
+              href='/about'
+              className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'
+            >
+              <li className='indent-[.5rem]'>About</li>
+            </Link>
+            <Link
+              href='/academics'
+              className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'
+            >
+              <li className='indent-[.5rem] '>Academics</li>
+            </Link>
+            <Link
+              href='/admissions'
+              className='border-b border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'
+            >
+              <li className='indent-[.5rem]'>Admissions</li>
+            </Link>
+            <Link
+              href='/campuslife'
+              className='border-[#ffffaa] font-bold text-xl w-[90%] p-[.75rem_0]'
+            >
+              <li className='indent-[.5rem]'>Campus Life</li>
+            </Link>
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 
--- app/components/NavSearch.jsx
+++ app/components/NavSearch.jsx
@@ -1,14 +1,48 @@
 import Nav from './Nav';
 import Input from './Input';
-
+import Link from 'next/link';
 
 const NavSearch = () => {
   return (
-    <div className='bg-[#109cde]'>
-      <div className='bg-[#019cde] h-[80px] w-full md:w-[20%] flex md:float-right justify-center items-center'>
+    <div className='bg-[#019cde] xl:bg-[#103356] xl:relative xl:flex xl:justify-center xl:py-2'>
+      <div className='bg-[#019cde] h-[80px] xl:h-[60px] w-full md:w-[168px] xl:w-[80px] flex md:float-right justify-center items-center xl:absolute xl:bottom-[-60px] xl:right-0'>
         <Input />
         <Nav className='w-[20%] md:order-[1]' />
       </div>
+      <nav className='hidden xl:block xl:w-[60%] margin-[0_auto]'>
+        <ul className=' w-full h-full text-white flex flex-row justify-between'>
+          <Link
+            href='/'
+            className='font-bold xl:font-normal p-[.75rem_0] text-[18px] cursor-pointer'
+          >
+            <li>Home</li>
+          </Link>
+          <Link
+            href='/about'
+            className='font-bold xl:font-normal p-[.75rem_0] text-[18px] cursor-pointer'
+          >
+            <li>About</li>
+          </Link>
+          <Link
+            href='/academics'
+            className='font-bold xl:font-normal p-[.75rem_0] text-[18px] cursor-pointer'
+          >
+            <li>Academics</li>
+          </Link>
+          <Link
+            href='/admissions'
+            className='font-bold xl:font-normal p-[.75rem_0] text-[18px] cursor-pointer'
+          >
+            <li>Admissions</li>
+          </Link>
+          <Link
+            href='/campuslife'
+            className='font-bold xl:font-normal p-[.75rem_0] text-[18px] cursor-pointer'
+          >
+            <li>Campus Life</li>
+          </Link>
+        </ul>
+      </nav>
     </div>
   );
 };
