b76148daebe7c7496c5fd6216a9c9d9d219aedba
osarodev <osernon@gmail.com>
2023-06-17 22:45:40 +0100

    add content to carousel

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -1,15 +1,30 @@
 import CarouselTemplate from './CarouselTemplate';
 
 const Carousel = () => {
-  const images = [
-    '/images/library.jpg',
-    '/images/classroom.jpg',
-    '/images/school.jpg',
+  const slides = [
+    {
+      src: '/images/library.jpg',
+      alt: 'library photo',
+      header: 'OUR LIBRARY',
+      para: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum?',
+    },
+    {
+      src: '/images/classroom.jpg',
+      alt: 'classroom photo',
+      header: 'OUR CLASSROOM',
+      para: 'Lorem, ipsum dolor sit amet consectetur adipisicing.',
+    },
+    {
+      src: '/images/school.jpg',
+      alt: 'school photo',
+      header: 'OUR SCHOOL BUILDING',
+      para: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
   ];
 
   return (
     <div className='container'>
-      <CarouselTemplate images={images} interval={5000} />
+      <CarouselTemplate slides={slides} interval={5000} />
     </div>
   );
 };
--- app/components/CarouselTemplate.jsx
+++ app/components/CarouselTemplate.jsx
@@ -4,43 +4,59 @@ import React, { useState, useEffect } from 'react';
 import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import Image from 'next/image';
 
-const CarouselTemplate = ({ images, interval = 3000 }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+const CarouselTemplate = ({ slides, interval }) => {
+  const [index, setIndex] = useState(0);
 
-  const nextImage = () => {
-    const newIndex = (currentImageIndex + 1) % images.length;
-    setCurrentImageIndex(newIndex);
+  const addIndex = () => {
+    return setIndex((prev) => {
+      if (prev === slides.length - 1) {
+        return 0;
+      } else {
+        return prev + 1;
+      }
+    });
   };
 
-  const previousImage = () => {
-    const newIndex = (currentImageIndex - 1 + images.length) % images.length;
-    setCurrentImageIndex(newIndex);
+  const subIndex = () => {
+    return setIndex((prev) => {
+      if (prev === 0) {
+        return slides.length - 1;
+      } else {
+        return prev - 1;
+      }
+    });
   };
 
   useEffect(() => {
-    const intervalId = setInterval(nextImage, interval);
+    const autoSlide = setInterval(addIndex, interval);
     return () => {
-      clearInterval(intervalId);
+      clearInterval(autoSlide);
     };
-  }, [currentImageIndex, interval]);
+  }, [index]);
 
   return (
-    <div className='border-y-4 border-[#f1c40f]'>
-      <section className='w-[375px] sm:w-[640px] md:w-[768px] lg:w-[1000px] xl:w-[1280px] 2xl:w-[1536px] h-[218px] sm:h-[372px] md:h-[446px] lg:h-[595px] xl:h[744px] 2xl:h-[893px] relative'>
-        <Image src={images[currentImageIndex]} fill={true} alt='' />
+    <div className='w-full flex flex-col justify-center items-center'>
+      <section className='w-[375px] sm:w-[640px] md:w-[768px] lg:w-[1024px] xl:w-[1280px] 2xl:w-[1516px] h-[180px] sm:h-[372px] md:h-[385px] lg:h-[480px] xl:h[480px] 2xl:h-[893px] relative'>
+        <Image src={slides[index].src} fill={true} alt={slides[index].alt} />
         <button
-          onClick={previousImage}
-          className='absolute left-[1rem] top-[50%] translate-y-[-50%] text-white text-4xl z-100'
+          onClick={subIndex}
+          className='absolute left-[.75rem] top-[50%] translate-y-[-50%] text-white text-4xl z-100'
         >
           <FiChevronLeft />
         </button>
         <button
-          onClick={nextImage}
-          className='absolute right-[1rem] top-[50%] translate-y-[-50%] text-white text-4xl z-100'
+          onClick={addIndex}
+          className='absolute right-[.75rem] top-[50%] translate-y-[-50%] text-white text-4xl z-100'
         >
           <FiChevronRight />
         </button>
       </section>
+      <div className='bg-[#252525] w-full text-center text-white p-[1.5rem]'>
+        <span className='border-l-4 border-[#f1c40f] text-[26px] pl-2'>
+          {slides[index].header}
+        </span>
+        <p className='pt-4'>{slides[index].para}</p>
+      </div>
     </div>
   );
 };
